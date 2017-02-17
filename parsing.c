7515b4ee8f9898c8b0873b9555611db6c7c71dd3
Luke Normile <luke.normile@outlook.com>
2017-02-16 18:05:33 -0700

    Basic operations and doge constructions implemented.

--- parsing.c
+++ parsing.c
@@ -28,6 +28,62 @@ void add_history(char *unused) {}
 #include <editline/history.h>
 #endif /* _WIN32 */
 
+/* Use operator string to see which operation to perform. */
+long eval_op(long x, char *op, long y) {
+	if (strcmp(op, "+") == 0) { return x + y; }
+	if (strcmp(op, "-") == 0) { return x - y; }
+	if (strcmp(op, "*") == 0) { return x * y; }
+	if (strcmp(op, "/") == 0) { return x / y; }
+	if (strcmp(op, "%") == 0) { return x % y; }
+	if (strcmp(op, "add") == 0) { return x + y; }
+	if (strcmp(op, "sub") == 0) { return x - y; }
+	if (strcmp(op, "mul") == 0) { return x * y; }
+	if (strcmp(op, "div") == 0) { return x / y; }
+	if (strcmp(op, "mod") == 0) { return x % y; }
+	return 0;
+}
+
+long eval_expr(mpc_ast_t* t) {
+	/* If tagged as number return it directly. */
+	if (strstr(t->tag, "number")) {
+		return atoi(t->contents);
+	}
+
+	/* The operator is always the second child. */
+	char *op = t->children[1]->contents;
+
+	/* We store the third child in 'x'/ */
+	long x = eval_expr(t->children[2]);
+
+	/* Iterate the remaining children and combining. */
+	int i = 3;
+	while(strstr(t->children[i]->tag, "expr")) {
+		x = eval_op(x, op, eval_expr(t->children[i]));
+		i++;
+	}
+
+	return x;
+}
+
+char *eval_doge(mpc_ast_t* t) {
+	return "Wow";
+}
+
+/* Begin to eval an input. */
+void eval(mpc_ast_t *t) {
+	/* Get the type of construction. */
+	mpc_ast_t *construction = t->children[1];
+
+	/* If tagged as an operator evaluate it as an expression. */
+	if (strstr(construction->tag, "operator")) {
+		long result = eval_expr(t);
+		printf("%li\n", result);
+	} else if (strstr(construction->tag, "doge")) {
+		char *doge_result = eval_doge(construction);
+		printf("%s\n", doge_result);
+	}
+}
+
 int main(int argc, char **argv) {
 	/* Create some parsers. */
 	mpc_parser_t* Doge_Adj    = mpc_new("doge_adj");
@@ -62,7 +118,7 @@ int main(int argc, char **argv) {
 	);
 
 	/* Print version and exit information. */
-	puts("lusp Version 0.0.0.0.1");
+	puts("lusp Version 0.0.0.0.3");
 	puts("Press Ctrl+c to Exit\n");
 
 	/* In an infinite loop... */
@@ -77,7 +133,7 @@ int main(int argc, char **argv) {
 		mpc_result_t r;
 		if(mpc_parse("<stdin>", input, Lusp, &r)) {
 			/* On success print the AST. */
-			mpc_ast_print(r.output);
+			eval(r.output);
 			mpc_ast_delete(r.output);
 		} else {
 			/* Otherwise print the error. */
